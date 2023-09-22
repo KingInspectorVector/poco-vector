e3b9ff582ce063bd7bed61edb24d562a09e85b1f
InspectorVector <viktor.khrapal@powercoders.org>
2023-09-22 11:39:59 +0200

    Create app and work it

---
+++ my-electron-app/main.js
@@ -0,0 +1,15 @@
+// main.js
+const { app, BrowserWindow } = require('electron')
+
+app.on('ready', () => {
+   const mainWindow = new BrowserWindow({
+       width: 800,
+       height: 600,
+       webPreferences: {
+           nodeIntegration: true
+       }
+   })
+
+   // Загрузите вашу HTML-страницу
+   mainWindow.loadFile('index.html')
+})
